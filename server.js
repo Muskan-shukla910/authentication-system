506e2e74f5a39e149316f78157ecd0a94c045337
Muskan Shukla <muskanshukla187@gmail.com>
2026-04-02 21:37:37 +0530

    sever.js

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+ const app=express();
+const port=process.env.PORT|| 4000
+
+
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors({credentials:true}));
+
+app.get('/',(req,res)=>{
+
+    res.cookie("name","Muskan");
+    
+    res.send("done");
+});
+
+app.get("/home",(req,res)=>{
+    console.log(req.cookies);
+    res.send("Hey I am Home page");
+});
+
+app.get("/login",(req,res)=>{
+    res.send("Login Page");
+})
+app.listen(port,()=>{
+    console.log(`Server is rujuning at port ${port}`);
+});
+
+
+
